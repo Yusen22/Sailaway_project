faaa33c8676e7fc68ca32f2db29ba418f4cbef95
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-22 01:40:11 +0700

    changed z index of nav

--- sailaway-project/src/components/Navbar.jsx
+++ sailaway-project/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
 
 
     return (
-        <nav className="min-w-full border-b-2 bg-white fixed top-0 space-x-5 h-16 py-5 px-10 flex text-center items-center z-10">
+        <nav className="min-w-full border-b-2 bg-white text-black fixed top-0 space-x-5 h-16 py-5 px-10 flex text-center items-center z-40">
             <NavLogo />
             <div className="flex w-full justify-between pl-10 pr-6">
                 <ul className="navlinks flex space-x-14 justify-center text-xl items-end">
