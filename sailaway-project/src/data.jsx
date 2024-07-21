032efeeaf643469ff8fae82cf84ba5f07c8246a5
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-22 01:37:46 +0700

    added travelCards to data. updated heroCol

--- sailaway-project/src/data.jsx
+++ sailaway-project/src/data.jsx
@@ -74,22 +74,47 @@ import heroImg3 from './assets/hero-img-3.jpg'
         {
             img: heroImg1,
             id: 1,
-            alt: '',
-            text: '1'
+            text: 'Find out the best rivers to see by boat',
+            alt: 'Boat in river at sunset',
         },
         {
             img: heroImg2,
             id: 2,
-            alt: '',
-            text: '2'
+            text: 'The changing fortunes of this seaside community',
+            alt: 'Long shot of sunny seaside town'
         },
         {
             img: heroImg3,
             id: 3,
-            alt: '',
-            text: '3'
+            text: 'Book now for Autumn 2024 Deals',
+            alt: 'Aerial shot of a winding road through trees in autumn'
         },
        
     ]
 
+    export const travelCards = [
+        {
+            img: '',
+            id: 1,
+            headerText: 'Barcelona',
+            text: 'A vibrant and lively city that is sure to have something for everyone. See the Sagrada Familia, or take part in the La Mercè festival.',
+            alt: 'Boat in river at sunset'
+        },
+        {
+            img: '',
+            id: 2,
+            headerText: 'Koh Rong',
+            text: 'Stunning island beaches off the coast of Cambodia. Come to relax experience, or find one of the many resort parties around the island',
+            alt: 'Boat in river at sunset'
+        },
+        {
+            img: '',
+            id: 3,
+            headerText: 'Prague',
+            text: "Prague is characterized by its beautiful medieval architecture, so visit the iconic Charles Bridge or Prague Castle while you're there",
+            alt: 'Boat in river at sunset'
+        },
+       
+       
+    ]
     
