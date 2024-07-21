ba54850f8071c20884f944f08146f0ba546d288a
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-21 14:05:50 +0700

    created data.js file and added arrays for header

---
+++ sailaway-project/src/data.jsx
@@ -0,0 +1,70 @@
+
+   export const navLinks = [
+        {
+            title: 'Home',
+            id: 1,
+            href: ''
+        },
+        {
+            title: 'Trips',
+            id: 2,
+            href: ''
+        },
+        {
+            title: 'Places',
+            id: 3,
+            href: ''
+        },
+        {
+            title: 'Blog',
+            id: 4,
+            href: ''
+        },
+    ]
+
+   export const sideNavLinks = [
+        {
+            title: 'About us',
+            id: 1,
+            img: 'info-circle',
+            alt: '',
+            href: ''
+        },
+        {
+            title: 'Contact',
+            id: 2,
+            img: 'envelope',
+            alt: '',
+            href: ''
+        },
+        {
+            title: 'Settings',
+            id: 3,
+            img: 'cog',
+            alt: '',
+            href: ''
+        },
+    ]
+
+   export const icons = [
+        {
+            img: 'twitter',
+            id: 1,
+            alt: '',
+            href: ''
+        },
+        {
+            img: 'facebook-square',
+            id: 2,
+            alt: '',
+            href: ''
+        },
+        {
+            img: 'instagram',
+            id: 3,
+            alt: '',
+            href: ''
+        }
+    ]
+
+    
