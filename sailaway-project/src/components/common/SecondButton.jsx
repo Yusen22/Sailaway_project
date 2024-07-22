436fe96ef26c3611f8248f370deb64060116d06b
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-22 18:06:08 +0700

    created SecondButton comp

---
+++ sailaway-project/src/components/common/SecondButton.jsx
@@ -0,0 +1,8 @@
+const SecondButton = ({children}) => {
+  return (
+    <>
+    <button className="w-fit px-8 py-4 border border-black/25 mb-1 mt-5 bg-white text-black hover:bg-white  hover:text-sky-600 rounded-lg uppercase font-extrabold tracking-widest transition ease-out duration-200">{children}</button>
+    </>
+  )
+}
+export default SecondButton
