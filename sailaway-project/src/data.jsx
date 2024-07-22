0dac821a4c24e5964be38e6bd115cd5736a91a1a
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-22 18:10:17 +0700

    created TripsData array in data. imported related img

--- sailaway-project/src/data.jsx
+++ sailaway-project/src/data.jsx
@@ -2,119 +2,195 @@ import heroImg1 from './assets/hero-img-1.jpg'
 import heroImg2 from './assets/hero-img-2.jpg'
 import heroImg3 from './assets/hero-img-3.jpg'
 
-   export const navLinks = [
-        {
-            title: 'Home',
-            id: 1,
-            href: ''
-        },
-        {
-            title: 'Trips',
-            id: 2,
-            href: ''
-        },
-        {
-            title: 'Places',
-            id: 3,
-            href: ''
-        },
-        {
-            title: 'Blog',
-            id: 4,
-            href: ''
-        },
-    ]
+import cardImg1 from './assets/travel-img-1.jpg'
+import cardImg2 from './assets/travel-img-2.jpg'
+import cardImg3 from './assets/travel-img-3.jpg'
 
-   export const sideNavLinks = [
-        {
-            title: 'About us',
-            id: 1,
-            img: 'info-circle',
-            alt: '',
-            href: ''
-        },
-        {
-            title: 'Contact',
-            id: 2,
-            img: 'envelope',
-            alt: '',
-            href: ''
-        },
-        {
-            title: 'Settings',
-            id: 3,
-            img: 'cog',
-            alt: '',
-            href: ''
-        },
-    ]
+import tripsImg1 from './assets/trips-img-1.jpg'
 
-   export const icons = [
-        {
-            img: 'twitter',
-            id: 1,
-            alt: '',
-            href: ''
-        },
-        {
-            img: 'facebook-square',
-            id: 2,
-            alt: '',
-            href: ''
-        },
-        {
-            img: 'instagram',
-            id: 3,
-            alt: '',
-            href: ''
-        }
-    ]
+export const navLinks = [
+    {
+        title: 'Home',
+        id: 1,
+        href: ''
+    },
+    {
+        title: 'Trips',
+        id: 2,
+        href: ''
+    },
+    {
+        title: 'Places',
+        id: 3,
+        href: ''
+    },
+    {
+        title: 'Blog',
+        id: 4,
+        href: ''
+    },
+]
 
-    export const heroColumn = [
-        {
-            img: heroImg1,
-            id: 1,
-            text: 'Find out the best rivers to see by boat',
-            alt: 'Boat in river at sunset',
-        },
-        {
-            img: heroImg2,
-            id: 2,
-            text: 'The changing fortunes of this seaside community',
-            alt: 'Long shot of sunny seaside town'
-        },
-        {
-            img: heroImg3,
-            id: 3,
-            text: 'Book now for Autumn 2024 Deals',
-            alt: 'Aerial shot of a winding road through trees in autumn'
-        },
-       
-    ]
+export const sideNavLinks = [
+    {
+        title: 'About us',
+        id: 1,
+        img: 'info-circle',
+        alt: '',
+        href: ''
+    },
+    {
+        title: 'Contact',
+        id: 2,
+        img: 'envelope',
+        alt: '',
+        href: ''
+    },
+    {
+        title: 'Settings',
+        id: 3,
+        img: 'cog',
+        alt: '',
+        href: ''
+    },
+]
 
-    export const travelCards = [
-        {
-            img: '',
-            id: 1,
-            headerText: 'Barcelona',
-            text: 'A vibrant and lively city that is sure to have something for everyone. See the Sagrada Familia, or take part in the La Mercè festival.',
-            alt: 'Boat in river at sunset'
-        },
-        {
-            img: '',
-            id: 2,
-            headerText: 'Koh Rong',
-            text: 'Stunning island beaches off the coast of Cambodia. Come to relax experience, or find one of the many resort parties around the island',
-            alt: 'Boat in river at sunset'
-        },
-        {
-            img: '',
-            id: 3,
-            headerText: 'Prague',
-            text: "Prague is characterized by its beautiful medieval architecture, so visit the iconic Charles Bridge or Prague Castle while you're there",
-            alt: 'Boat in river at sunset'
-        },
-       
-       
-    ]
-    
+export const icons = [
+    {
+        img: 'twitter',
+        id: 1,
+        alt: '',
+        href: ''
+    },
+    {
+        img: 'facebook-square',
+        id: 2,
+        alt: '',
+        href: ''
+    },
+    {
+        img: 'instagram',
+        id: 3,
+        alt: '',
+        href: ''
+    }
+]
+
+export const heroColumn = [
+    {
+        img: heroImg1,
+        id: 1,
+        text: 'Find out the best rivers to see by boat',
+        alt: 'Boat in river at sunset',
+    },
+    {
+        img: heroImg2,
+        id: 2,
+        text: 'The changing fortunes of this seaside community',
+        alt: 'Long shot of sunny seaside town'
+    },
+    {
+        img: heroImg3,
+        id: 3,
+        text: 'Book now for Autumn 2024 Deals',
+        alt: 'Aerial shot of a winding road through trees in autumn'
+    },
+
+]
+
+export const travelCards = [
+    {
+        img: cardImg1,
+        id: 1,
+        headerText: 'Barcelona',
+        text: 'A vibrant and lively city that is sure to have something for everyone. See the Sagrada Familia, or take part in the La Mercè festival.',
+        alt: 'Boat in river at sunset'
+    },
+    {
+        img: cardImg2,
+        id: 2,
+        headerText: 'Koh Rong',
+        text: 'Stunning island beaches off the coast of Cambodia. Come to relax experience, or find one of the many resort parties around the island',
+        alt: 'Boat in river at sunset'
+    },
+    {
+        img: cardImg3,
+        id: 3,
+        headerText: 'Prague',
+        text: "Prague is characterized by its beautiful medieval architecture, so visit the iconic Charles Bridge or Prague Castle while you're there",
+        alt: 'Boat in river at sunset'
+    },
+]
+
+export const TripsData = [
+    {
+        img: tripsImg1,
+        alt: 'Rio de Janeiro',
+        imgText: 'December 12th 2024',
+        title: 'Rio Exploration',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit facilis, atque blanditiis numquam officiis aliquam harum beatae et sequi totam quaerat modi? Inventore numquam commodi, ratione tempora aspernatur necessitatibus quia?',
+        country: 'Brazil',
+        duration: '14 Days',
+        cost: '$1100',
+        id: 1,
+    },
+    {
+        img: tripsImg1,
+        alt: 'Rio de Janeiro',
+        imgText: 'December 12th 2024',
+        title: 'Rio Exploration',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit facilis, atque blanditiis numquam officiis aliquam harum beatae et sequi totam quaerat modi? Inventore numquam commodi, ratione tempora aspernatur necessitatibus quia?',
+        country: 'Brazil',
+        duration: '14 Days',
+        cost: '$1100',
+        id: 2,
+    },
+    {
+        img: tripsImg1,
+        alt: 'Rio de Janeiro',
+        imgText: 'December 12th 2024',
+        title: 'Rio Exploration',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit facilis, atque blanditiis numquam officiis aliquam harum beatae et sequi totam quaerat modi? Inventore numquam commodi, ratione tempora aspernatur necessitatibus quia?',
+        country: 'Brazil',
+        duration: '14 Days',
+        cost: '$1100',
+        id: 3,
+    },
+    {
+        img: tripsImg1,
+        alt: 'Rio de Janeiro',
+        imgText: 'December 12th 2024',
+        title: 'Rio Exploration',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit facilis, atque blanditiis numquam officiis aliquam harum beatae et sequi totam quaerat modi? Inventore numquam commodi, ratione tempora aspernatur necessitatibus quia?',
+        country: 'Brazil',
+        duration: '14 Days',
+        cost: '$1100',
+        id: 4,
+    },
+    {
+        img: tripsImg1,
+        alt: 'Rio de Janeiro',
+        imgText: 'December 12th 2024',
+        title: 'Rio Exploration',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit facilis, atque blanditiis numquam officiis aliquam harum beatae et sequi totam quaerat modi? Inventore numquam commodi, ratione tempora aspernatur necessitatibus quia?',
+        country: 'Brazil',
+        duration: '14 Days',
+        cost: '$1100',
+        id: 5,
+    },
+    {
+        img: tripsImg1,
+        alt: 'Rio de Janeiro',
+        imgText: 'December 12th 2024',
+        title: 'Rio Exploration',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit facilis, atque blanditiis numquam officiis aliquam harum beatae et sequi totam quaerat modi? Inventore numquam commodi, ratione tempora aspernatur necessitatibus quia?',
+        country: 'Brazil',
+        duration: '14 Days',
+        cost: '$1100',
+        id: 6,
+    },
+
+
+
+]
+
