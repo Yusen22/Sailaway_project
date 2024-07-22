06abfceaf27f2edbec2a3bf566a86d85b1576609
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-23 00:56:28 +0700

    started Contact section

--- sailaway-project/src/components/Contact.jsx
+++ sailaway-project/src/components/Contact.jsx
@@ -1,6 +1,11 @@
+import ContactBg from '../assets/contact-bg.jpg'
+
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
+    return (
+        <div className='h-96 w-screen'>
+            <h3>Contact Us</h3>
+            <img className='h-full w-full object-cover' src={ContactBg}></img>
+        </div>
+    )
 }
 export default Contact
