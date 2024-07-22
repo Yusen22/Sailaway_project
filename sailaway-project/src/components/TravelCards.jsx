45b43cc1091f375f84c1e72e2040e55860617361
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-22 18:12:03 +0700

    added Slider to TravelCards

--- sailaway-project/src/components/TravelCards.jsx
+++ sailaway-project/src/components/TravelCards.jsx
@@ -1,14 +1,18 @@
 import { travelCards } from "../data"
+import SimpleSlider from "./common/Slider"
+import Button from "./common/Button"
 
 const TravelCards = () => {
 
-    const TravelCard = ({ img, id, headerText, text, alt } ) => {
+    const TravelCard = ({ img, id, headerText, text, alt }) => {
         return (
             <div className="card" key={id}>
-                <img src={img} alt={alt} />
-                <h5>{headerText}</h5>
+                <img src={img} alt={alt} className="rounded-3xl" />
+                <h5 className="uppercase font-bold text-lg tracking-widest py-4">{headerText}</h5>
                 <p>{text}</p>
-                <button></button>
+                <Button>
+                    Ok
+                </Button>
             </div>
         )
     }
@@ -16,23 +20,26 @@ const TravelCards = () => {
 
     return (
         <section className="bg-white w-screen h-full px-48">
-            <div className="py-14">
+            <div className="pt-14">
                 <h5 className="text-gray-500 text-left pb-2">Start planning</h5>
-                <h2 className="text-left text-black text-6xl " style={{ fontFamily: 'Lexend' }}>Where do you want to go?</h2>
+                <h2 className="text-left text-black text-6xl underline" style={{ fontFamily: 'Lexend' }}>Where do you want to go?</h2>
             </div>
-            <div className="w-full h-[vh-80] grid grid-cols-3 gap-6 z-40 my-20 text-black">
+            <div className="w-full h-[vh-80] gap-8 z-40 my-20 text-black">
                 {console.log(travelCards)}
-                {travelCards.map((card, i) => {
-                    return <TravelCard
-                        key={card.id}
-                        img={card.img}
-                        id={card.id}
-                        headerText={card.headerText}
-                        text={card.text}
-                        alt={card.alt}
-                    />
-                }
-                )}
+                <SimpleSlider>
+                    {travelCards.map((card, i) => {
+                        return <TravelCard
+                            key={card.id}
+                            img={card.img}
+                            id={card.id}
+                            headerText={card.headerText}
+                            text={card.text}
+                            alt={card.alt}
+                        />
+
+                    }
+                    )}
+                </SimpleSlider>
             </div>
 
         </section>
