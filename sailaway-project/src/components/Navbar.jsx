6c12b1a1970ba1361649cf8530f57d7b34df170a
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-23 00:54:46 +0700

    implemented new NavLogo on navbar

--- sailaway-project/src/components/Navbar.jsx
+++ sailaway-project/src/components/Navbar.jsx
@@ -6,36 +6,40 @@ const Navbar = () => {
 
 
     return (
-        <nav className="min-w-full border-b-2 bg-white text-black fixed top-0 space-x-5 h-16 py-5 px-10 flex text-center items-center z-40">
-            <NavLogo />
+        <header className="min-w-full border-b-2 bg-white text-black fixed top-0 space-x-5 h-16 py-5 px-10 flex text-center items-center z-40">
+            <NavLogo size='3xl' logoWidth='12'/>
             <div className="flex w-full justify-between pl-10 pr-6">
-                <ul className="navlinks flex space-x-14 justify-center text-xl items-end">
-                    {console.log(icons[0])}
-                    {navLinks.map((list, i) => {
-                        return (
-                            <li key={list.id}>
-                                <a href={list.href}>
-                                    <h3 className="font-extrabold">{list.title}</h3>
-                                </a>
-                            </li>
-                        )
-                    })}
-                </ul>
-                <ul className="side-navlinks flex space-x-8 justify-center items-center">
-                    {console.log(icons[0])}
-                    {sideNavLinks.map((list, i) => {
-                        return (
-                            <li key={list.id}>
-                                <a className="flex items-center text-sm" href={list.href}>
-                                    <i className={`fa fa-${list.img} pr-2`} ></i>
-                                    <h3>{list.title}</h3>
-                                </a>
-                            </li>
-                        )
-                    })}
-                </ul>
+                <nav>
+                    <ul className="navlinks flex space-x-14 justify-center text-xl items-end">
+                        {console.log(icons[0])}
+                        {navLinks.map((list, i) => {
+                            return (
+                                <li key={list.id}>
+                                    <a href={list.href}>
+                                        <h3 className="font-extrabold">{list.title}</h3>
+                                    </a>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </nav>
+                <nav>
+                    <ul className="side-navlinks flex space-x-8 justify-center items-center">
+                        {console.log(icons[0])}
+                        {sideNavLinks.map((list, i) => {
+                            return (
+                                <li key={list.id}>
+                                    <a className="flex items-center text-sm" href={list.href}>
+                                        <i className={`fa fa-${list.img} pr-2`} ></i>
+                                        <h3>{list.title}</h3>
+                                    </a>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </nav>
             </div>
-        </nav>
+        </header>
     )
 }
 
