cbb0b17b1b448e0a6118dc401e898b587e315674
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-22 18:06:40 +0700

    created searchbar component

---
+++ sailaway-project/src/components/common/SearchBar.jsx
@@ -0,0 +1,13 @@
+const SearchBar = () => {
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Search..."
+        className="w-full border rounded-xl px-4 py-2 mt-3"
+
+      />
+    </form>
+  )
+}
+export default SearchBar
