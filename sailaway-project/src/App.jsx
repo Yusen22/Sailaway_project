922b6eb765eec85bd4937034319c4f82c2709ee2
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-22 18:10:55 +0700

    imported new components to App.jsx

--- sailaway-project/src/App.jsx
+++ sailaway-project/src/App.jsx
@@ -1,17 +1,30 @@
 
 import './App.css'
+import 'animate.css';
 
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import TravelCards from './components/TravelCards'
+import VideoSection from './components/VideoSection';
+import Trips from './components/Trips';
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+
 
 function App() {
 
   return (
     <>
+      <div className='relative w-screen'>
       <Navbar></Navbar>
       <Hero></Hero>
+      </div>
       <TravelCards></TravelCards>
+      <VideoSection></VideoSection>
+      <Trips></Trips>
+      <Contact></Contact>
+      <Footer></Footer>
     </>
   )
 }
